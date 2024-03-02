7304536bfb32b16f5a9a717caa9e81e30d533ac5
Kneba <abenkenary3@gmail.com>
2024-03-02 12:36:22 +0700

    reupdate

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@
 # Function to show an informational message
 #
 
+#set -e
+
 msg() {
-	echo
     echo -e "\e[1;32m$*\e[0m"
-    echo
 }
 
 err() {
@@ -21,6 +21,9 @@ cdir() {
 	cd "$1" 2>/dev/null || \
 		err "The directory $1 doesn't exists !"
 }
+
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
 
 # Main
 MainPath="$(pwd)"
@@ -45,7 +48,9 @@ ZIPNAME="$KERNELNAME-Kernel-4-19-KSU"
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Clone Kernel Source
-git clone --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm660 -b tom/u kernel
+echo " "
+msg "|| Cloning Kernel Source ||"
+git clone --depth=1 --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm660 -b tom/u kernel
 
 # Clone AOSP Clang
 ClangPath=${MainClangZipPath}
@@ -53,6 +58,7 @@ ClangPath=${MainClangZipPath}
 rm -rf $ClangPath/*
 mkdir $ClangPath
 
+msg "|| Cloning AOSP Clang ||"
 #git clone --depth=1 https://gitlab.com/ImSurajxD/clang-r450784d -b master $ClangPath
 wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r487747c.tar.gz -O "clang-r487747c.tar.gz"
 tar -xf clang-r487747c.tar.gz -C $ClangPath
@@ -62,10 +68,14 @@ rm -rf $GCCaPath/*
 rm -rf $GCCbPath/*
 mkdir $GCCaPath
 mkdir $GCCbPath
+msg "|| Cloning AOSP GCC ||"
 wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc64.tar.gz"
 tar -xf gcc64.tar.gz -C $GCCaPath
 wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc32.tar.gz"
 tar -xf gcc32.tar.gz -C $GCCbPath
+
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
 
 # Prepared
 KERNEL_ROOTDIR=$(pwd)/kernel # IMPORTANT ! Fill with your kernel source root directory.
@@ -82,8 +92,24 @@ START=$(date +"%s")
 # Java
 command -v java > /dev/null 2>&1
 
+# Check Kernel Version
+KERVER=$(make kernelversion)
+
 # Telegram
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+export BOT_BUILD_URL="https://api.telegram.org/bot$TG_TOKEN/sendDocument"
+
+tg_post_build() {
+    #Post MD5Checksum alongwith for easeness
+    MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
+    #Show the Checksum alongwith caption
+    curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+    -F chat_id="$2"  \
+    -F "disable_web_page_preview=true" \
+    -F "parse_mode=html" \
+    -F caption="$3 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"  
+}
 
 # Telegram messaging
 tg_post_msg() {
@@ -97,6 +123,7 @@ compile(){
 cd ${KERNEL_ROOTDIR}
 export HASH_HEAD=$(git rev-parse --short HEAD)
 export COMMIT_HEAD=$(git log --oneline -1)
+msg "|| Compile starting ||"
 make -j$(nproc) O=out ARCH=arm64 asus/X00TD_defconfig
 make -j$(nproc) ARCH=arm64 O=out \
     LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}" \
@@ -127,7 +154,7 @@ make -j$(nproc) ARCH=arm64 O=out \
 # Push kernel to telegram
 function push() {
     cd AnyKernel
-    curl -F document="@$ZIP_FINAL.zip" "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+    curl -F document="@$ZIP_FINAL.zip" "$BOT_BUILD_URL" \
         -F chat_id="$TG_CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
@@ -136,7 +163,7 @@ function push() {
         <b>📅 Build Date: </b>
         -<code>$DATE</code>
         <b>🐧 Linux Version: </b>
-        -<code>4.19.308</code>
+        -<code>$KERVER</code>
          <b>💿 Compiler: </b>
         -<code>$KBUILD_COMPILER_STRING</code>
         <b>📱 Device: </b>
@@ -148,7 +175,7 @@ function push() {
 }
 # Find Error
 function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+    curl -s -X POST "$BOT_MSG_URL" \
         -d chat_id="$TG_CHAT_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
