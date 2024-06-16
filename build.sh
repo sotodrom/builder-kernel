f9cc2e269d746baf91a6bd8f9d8f5ee2b298c367
Kneba <abenkenary3@gmail.com>
2024-06-17 03:15:06 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ VARIANT=EAS
 BASE=kernel-common
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="$KERNELNAME-UDC-4-19-312"
+ZIPNAME="$KERNELNAME-UDC-4-19-TEST"
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
@@ -46,7 +46,7 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Clone Kernel Source
 echo " "
 msg "|| Cloning Kernel Source ||"
-git clone --depth=1 --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm660 -b tree kernel
+git clone --depth=1 --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm660 -b staging kernel
 
 # Clone AOSP Clang
 [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
